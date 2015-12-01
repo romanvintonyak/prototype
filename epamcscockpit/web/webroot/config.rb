cc16b6c3e66c69d395b9cfcdc5a94985ae990f00
camom1le <camom1le@github.com>
2015-12-01 12:08:05 +0200

    CSS configuration using compass. CSS updated

---
+++ epamcscockpit/web/webroot/config.rb
@@ -0,0 +1,10 @@
+require 'susy'
+
+relative_assets = true
+
+http_images_path = "loool"
+css_dir = "styles"
+sass_dir = "styles"
+images_dir = "img"
+line_comments = false
+sass_options = {:unix_newlines => true}
