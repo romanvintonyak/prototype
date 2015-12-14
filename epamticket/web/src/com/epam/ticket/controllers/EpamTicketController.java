828d197b923f89ef7fd559540dddb4207a70ad51
Dmytro_Adonin <dmytro_adonin@epam.com>
2015-12-14 18:17:14 +0200

    Rest MVC controller test infrastructure adjusted, functionality for ticket creation and getting ticket by ID covered with test

--- epamticket/web/src/com/epam/ticket/controllers/EpamTicketController.java
+++ epamticket/web/src/com/epam/ticket/controllers/EpamTicketController.java
@@ -1,35 +1,21 @@
 package com.epam.ticket.controllers;
 
-import com.epam.ticket.data.EpamCustomerEvent;
+import com.epam.ticket.dao.EpamTicketDAO.TicketCountsResult;
 import com.epam.ticket.data.EpamNewTicket;
 import com.epam.ticket.data.EpamTicket;
 import com.epam.ticket.facades.EpamTicketSearchCriteria;
 import com.epam.ticket.facades.impl.DefaultEpamTicketFacade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.io.Serializable;
 import java.util.Collection;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.epam.ticket.dao.EpamTicketDAO.TicketCountsResult;
-import com.epam.ticket.data.EpamTicket;
-import com.epam.ticket.facades.EpamTicketSearchCriteria;
-import com.epam.ticket.facades.impl.DefaultEpamTicketFacade;
-
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@Controller
+@RestController
 @RequestMapping("/v1/tickets")
 public class EpamTicketController {
     
@@ -37,25 +23,21 @@ public class EpamTicketController {
     private DefaultEpamTicketFacade defaultEpamTicketFacade;
 
     @RequestMapping(method = GET)
-    @ResponseBody
     public Collection<EpamTicket> getTicketsByCriteria(EpamTicketSearchCriteria searchCriteria) {
         return defaultEpamTicketFacade.getTicketsByCriteria(searchCriteria);
     }
 
     @RequestMapping(method = POST, consumes = APPLICATION_JSON_VALUE)
-    @ResponseBody
     public EpamTicket addTicket(@RequestBody EpamNewTicket ticket) {
         return defaultEpamTicketFacade.addTicket(ticket.getNewTicket(), ticket.getCreationEvent());
     }
 
     @RequestMapping(value = "/{ticketId}", method = GET)
-    @ResponseBody
     public EpamTicket getTicket(@PathVariable("ticketId") String ticketId) {
         return defaultEpamTicketFacade.getTicketById(ticketId);
     }
 
     @RequestMapping(value = "/ticketCount", method = GET)
-    @ResponseBody
     public TicketCounterHolder getTicketCount() {
         TicketCounterHolder ticketCounterHolder = new TicketCounterHolder();
         ticketCounterHolder.setTotal(defaultEpamTicketFacade.getTotalTicketCount());
@@ -63,7 +45,6 @@ public class EpamTicketController {
     }
 
     @RequestMapping(value = "/ticketCounts", method = RequestMethod.GET)
-    @ResponseBody
     public TicketCountsResult getTicketCounts(@RequestParam(value = "userName", required = false, defaultValue = "csagent") String userName) {
         // TODO: GET RID of userName, when security will be ready!
         return defaultEpamTicketFacade.getTicketCounts(userName);
