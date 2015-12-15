9edcdbd7f2584a6215283f23a2f857d7beb5b360
Oleksiy_Tyshchenko <oleksiy.tyshchenko@epam.com>
2015-12-15 13:50:58 +0200

    added todos

--- epamcscockpit/web/src/com/epam/controllers/EpamTicketController.java
+++ epamcscockpit/web/src/com/epam/controllers/EpamTicketController.java
@@ -17,11 +17,14 @@ public class EpamTicketController {
     @ResponseBody
     public List<EpamTicket> getAll() {
         return restHelper.call("http://localhost:9001/epamticket/v1/tickets", List.class);
+        // TODO: 12/15/2015 need to cover base context url
     }
 
     @RequestMapping(value = "/{ticketId}", method = RequestMethod.GET)
     @ResponseBody
     public EpamTicket getTicketById(@PathVariable("ticketId") final String ticketId) {
-        return restHelper.call("http://localhost:9001/epamticket/v1/tickets/" + ticketId, EpamTicket.class);
+        return restHelper.call(String.format("http://localhost:9001/epamticket/v1/tickets/%s", ticketId), EpamTicket.class);
+        // TODO: 12/15/2015 need to cover base context url
+
     }
 }
